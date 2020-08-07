6524c41307a0f56adda9b36778d1f97398dc5b3c
Richard Lee <richard.lee@steelseries.com>
2020-08-07 13:17:54 -0500

    Run gofmt on all files. No other changes

--- dbconf.go
+++ dbconf.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.com/erh/go-gypsy/yaml"
-    _ "github.com/mattn/go-sqlite3"
-    "os"
-    "path/filepath"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/erh/go-gypsy/yaml"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // global options. available to any subcommands.
@@ -14,39 +15,39 @@ var dbPath = flag.String("path", "db", "folder containing db info")
 var dbEnv = flag.String("env", "development", "which DB environment to use")
 
 type DBConf struct {
-    MigrationsDir string
-    Env           string
-    Driver        string
-    OpenStr       string
+	MigrationsDir string
+	Env           string
+	Driver        string
+	OpenStr       string
 }
 
 // extract configuration details from the given file
 func MakeDBConf() (*DBConf, error) {
 
-    cfgFile := filepath.Join(*dbPath, "dbconf.yml")
+	cfgFile := filepath.Join(*dbPath, "dbconf.yml")
 
-    f, err := yaml.ReadFile(cfgFile)
-    if err != nil {
-        return nil, err
-    }
+	f, err := yaml.ReadFile(cfgFile)
+	if err != nil {
+		return nil, err
+	}
 
-    drv, derr := f.Get(fmt.Sprintf("%s.driver", *dbEnv))
-    if derr != nil {
-        return nil, derr
-    }
+	drv, derr := f.Get(fmt.Sprintf("%s.driver", *dbEnv))
+	if derr != nil {
+		return nil, derr
+	}
 
-    open, oerr := f.Get(fmt.Sprintf("%s.open", *dbEnv))
-    if oerr != nil {
-        return nil, oerr
-    }
-    open = os.ExpandEnv(open)
+	open, oerr := f.Get(fmt.Sprintf("%s.open", *dbEnv))
+	if oerr != nil {
+		return nil, oerr
+	}
+	open = os.ExpandEnv(open)
 
-    // Automatically parse postgres urls
+	// Automatically parse postgres urls
 
-    return &DBConf{
-        MigrationsDir: filepath.Join(*dbPath, "migrations"),
-        Env:           *dbEnv,
-        Driver:        drv,
-        OpenStr:       open,
-    }, nil
+	return &DBConf{
+		MigrationsDir: filepath.Join(*dbPath, "migrations"),
+		Env:           *dbEnv,
+		Driver:        drv,
+		OpenStr:       open,
+	}, nil
 }
--- migration_go.go
+++ migration_go.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "text/template"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"text/template"
 )
 
 type TemplateData struct {
-    Version   int64
-    DBDriver  string
-    DBOpen    string
-    Direction int
-    Func      string
+	Version   int64
+	DBDriver  string
+	DBOpen    string
+	Direction int
+	Func      string
 }
 
 //
@@ -27,43 +27,43 @@ type TemplateData struct {
 //
 func runGoMigration(conf *DBConf, path string, version int64, direction int) error {
 
-    // everything gets written to a temp dir, and zapped afterwards
-    d, e := ioutil.TempDir("", "goose")
-    if e != nil {
-        log.Fatal(e)
-    }
-    defer os.RemoveAll(d)
+	// everything gets written to a temp dir, and zapped afterwards
+	d, e := ioutil.TempDir("", "goose")
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer os.RemoveAll(d)
 
-    directionStr := "Down"
-    if direction == 1 {
-        directionStr = "Up"
-    }
+	directionStr := "Down"
+	if direction == 1 {
+		directionStr = "Up"
+	}
 
-    td := &TemplateData{
-        Version:   version,
-        DBDriver:  conf.Driver,
-        DBOpen:    conf.OpenStr,
-        Direction: direction,
-        Func:      fmt.Sprintf("%v_%v", directionStr, version),
-    }
-    main, e := writeTemplateToFile(filepath.Join(d, "goose_main.go"), goMigrationTmpl, td)
-    if e != nil {
-        log.Fatal(e)
-    }
+	td := &TemplateData{
+		Version:   version,
+		DBDriver:  conf.Driver,
+		DBOpen:    conf.OpenStr,
+		Direction: direction,
+		Func:      fmt.Sprintf("%v_%v", directionStr, version),
+	}
+	main, e := writeTemplateToFile(filepath.Join(d, "goose_main.go"), goMigrationTmpl, td)
+	if e != nil {
+		log.Fatal(e)
+	}
 
-    outpath := filepath.Join(d, filepath.Base(path))
-    if _, e = copyFile(outpath, path); e != nil {
-        log.Fatal(e)
-    }
+	outpath := filepath.Join(d, filepath.Base(path))
+	if _, e = copyFile(outpath, path); e != nil {
+		log.Fatal(e)
+	}
 
-    cmd := exec.Command("go", "run", main, outpath)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    if e = cmd.Run(); e != nil {
-        log.Fatal("`go run` failed: ", e)
-    }
+	cmd := exec.Command("go", "run", main, outpath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if e = cmd.Run(); e != nil {
+		log.Fatal("`go run` failed: ", e)
+	}
 
-    return nil
+	return nil
 }
 
 //
